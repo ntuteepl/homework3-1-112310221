bc3dfd72b10eec8cf5a186cbc376dafa2143940e
112310221 <146065074+112310221@users.noreply.github.com>
2023-10-22 22:52:16 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -3,20 +3,20 @@
 
 int main()
 {
-int s1,d1,s2,d2,s3,d3,used,unused,i;
-scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
-      for(i = 1; i <= 24 ; i++){
-        if(i == d1 || i == d2 || i == d3){
-            unused += 1;
-        }
-        if(i == s1 || i == s2 || i == s3){
-            if(unused == 0){
-                used += 1;
-            }else{
-                unused -= 1;
+    int s1, d1, s2, d2, s3, d3, used = 0, i;
+    scanf("%d%d%d%d%d%d", &s1, &d1, &s2, &d2, &s3, &d3);
+    
+    for (i = 1; i <= 24; i++) {
+        if (i == d1 || i == d2 || i == d3) {
+            if (used > 0) {
+                used--;
             }
         }
+        if (i == s1 || i == s2 || i == s3) {
+            used++;
+        }
     }
-    printf("%d\n",used);
-    }
-
+    
+    printf("%d", used);
+    return 0;
+}
