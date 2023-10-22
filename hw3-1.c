f4c00e1b60a5bfafb65fca40798781a5eb207ca2
112310221 <146065074+112310221@users.noreply.github.com>
2023-10-22 22:47:40 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+//質數=>1和自己
+    int i,j,k;
+    scanf("%d",&i);
+    for (j=2;i>j;j++){
+        if (i%j==0){
+        k=1;
+        }
+    }
+    if (i<=1){
+        k=1;
+        }
+        if(k==1){
+            printf("NO");
+        }
+        else{
+            printf("YES");
+        }
+}
+//判斷質數
